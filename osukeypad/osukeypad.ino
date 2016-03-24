f1b11ee7747a676e5b9baabb82dee48d0e5b1887
thnikk <thnikkshop@gmail.com>
2016-03-23 19:54:38 -0700

    Basic/basic+ cleanup
    Moved the updated code to this repo since the model will be replaced
    with the new pro-micro based model.

--- osukeypad/osukeypad.ino
+++ osukeypad/osukeypad.ino
@@ -1,22 +1,21 @@
-#include <TrinketKeyboard.h> // keyboard library
-#include <avr/power.h> // for running at 16mhz
+#include <TrinketKeyboard.h> // Keyboard library
+#include <avr/power.h> // For OC
 #define KEY_1 0
 #define KEY_2 2
 #define KEY_3 1
 
+/* Reference keycodes available on Adafruit's
+github here: http://tinyurl.com/jfyybfn ****/
 #define KEYP_1 KEYCODE_Z
 #define KEYP_2 KEYCODE_X
 #define KEYP_3 KEYCODE_ESC
 
 void setup()
 {
-  if (F_CPU == 16000000) clock_prescale_set(clock_div_1); // 16mhz
-  pinMode(KEY_1, INPUT);
-  pinMode(KEY_2, INPUT);
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1); // Overclocks to 16mhz (probably unnecessary)
+  pinMode(KEY_1, INPUT_PULLUP);
+  pinMode(KEY_2, INPUT_PULLUP);
   pinMode(KEY_3, INPUT);
-  digitalWrite(KEY_1, HIGH); 
-  digitalWrite(KEY_2, HIGH);
-  digitalWrite(KEY_3, LOW);
   TrinketKeyboard.begin();
 }
 
@@ -24,7 +23,7 @@ void loop(){
   TrinketKeyboard.poll();
   
   if(digitalRead(KEY_3)){
-    TrinketKeyboard.pressKey(0, KEYP_3);  // side button gets precident
+    TrinketKeyboard.pressKey(0, KEYP_3);  // Side button gets precident
   }
   else if(!digitalRead(KEY_1) && !digitalRead(KEY_2)){
     TrinketKeyboard.pressKey(0, KEYP_1, KEYP_2);  // z and x can be pressed at the same time
@@ -36,6 +35,6 @@ void loop(){
     TrinketKeyboard.pressKey(0, KEYP_2);
   }
   else{
-    TrinketKeyboard.pressKey(0,0);  // needed so key is not pressed forever
+    TrinketKeyboard.pressKey(0,0);  // Needed so key is not pressed forever
   }  
 }
